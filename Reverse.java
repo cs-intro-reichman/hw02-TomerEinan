a2f08dc41232afc1b0c92dd58c37069f02ab1af0
TomerEinan <154810313+TomerEinan@users.noreply.github.com>
2024-01-04 23:48:39 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,15 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String word = args[0];
+		String reverseword = "";
+		int number = word.length();
+		System.out.println(number);
+		for (int i = number - 1; i > -1; i--){
+			reverseword = reverseword + word.charAt(i);
+		}
+		System.out.println(reverseword);
+		number = number / 2 + 1 - number % 2;
+		System.out.println("The middle character is " + word.charAt(number));
 	}
 }
