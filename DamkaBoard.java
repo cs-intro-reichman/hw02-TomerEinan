0635ada53b1900fc50ea7dddd8865d653eb1f15c
TomerEinan <154810313+TomerEinan@users.noreply.github.com>
2024-01-04 23:46:52 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,5 +4,16 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
+		int number = Integer.parseInt(args[0]);
+		for (int i = 0; i < number; i++){
+			System.out.println("");
+			if (i % 2 == 1){
+				System.out.print(" ");
+			}
+			for (int k = 0; k < number; k++){
+				System.out.print("* ");
+			}
+		}
+		System.out.println("");
 	}
 }
